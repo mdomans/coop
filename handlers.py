86e1dc525ebbb6c436a3c10f8f3bdc0d19e02a9a
Michał Domański <michal.domanski@sensisoft.com>
2009-11-23 00:46:21 +0100

    code works now, tough is very simple, need to explot further libev abilities

--- handlers.py
+++ handlers.py
@@ -7,12 +7,27 @@ class Handler(object):
     def install(self, reactor):
         raise NotImplementedError
 
+def handle_exit(watcher, events):
+    print("got SIGINT")
+    # optional - stop all watchers
+    if watcher.data:
+        print("stopping watchers: {0}".format(watcher.data))
+        for w in watcher.data:
+            w.stop()
+    # unloop all nested loop
+    print("stopping the loop: {0}".format(watcher.loop))
+    watcher.loop.unloop()
+
 class SIGINTHandler(Handler):
     sig = signal.SIGINT
     
-    def __init__(self, callback):
+    def __init__(self, callback=handle_exit):
         self.cb = callback
+        self._signal = None
         
     def install(self, reactor):
-        self._signal = pyev.Signal(self.sig, reactor._loop, self.cb)
+        #signal = pyev.Signal(handlers.SIGINTHandler.sig, self._loop, handlers.handle_exit)
+        self._signal = pyev.Signal(self.sig, reactor._loop, handle_exit)
         self._signal.start()
+        
+    
--- reactor.py
+++ reactor.py
@@ -4,38 +4,28 @@ import time
 from handlers import SIGINTHandler
 from events import Event
 
+
+def dispatch(watcher, events):
+    print('switching')
+    current = greenlet.getcurrent()
+    current.switch()
+
 class Reactor(object):
     
     def __init__(self):
-        self._current = greenlet.getcurrent()
         self._loop = pyev.default_loop()
         self._queue = []
         
     def run(self):
         #create dispatch event
-        Event(0, 2, self.dispatch).install(self)
         #create CTRL-C handler
-        SIGINTHandler(self.handle_exit).install(self)
+        sigint_handler = SIGINTHandler()
+        sigint_handler.install(self)
+        dispatch_event = Event(0,2,dispatch)
+        dispatch_event.install(self)
         #run loop
         self._loop.loop()
-        
-    def dispatch(self, watcher, events):
-        print('switching')
-        current = self._queue.pop(0)
-        current.switch()
-        self._queue.append(current)
-        
+
     def add(self, function):
         self._queue.append(greenlet.greenlet(function))
         
-
-    def handle_exit(self, watcher, events):
-        print("got SIGINT")
-        # optional - stop all watchers
-        if watcher.data:
-            print("stopping watchers: {0}".format(watcher.data))
-            for w in watcher.data:
-                w.stop()
-        # unloop all nested loop
-        print("stopping the loop: {0}".format(watcher.loop))
-        watcher.loop.unloop()
