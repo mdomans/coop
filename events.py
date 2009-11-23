d55a932cd867043882c679780108346412cb12fb
Michał Domański <michal.domanski@sensisoft.com>
2009-11-23 11:17:57 +0100

    added some events, fixed tests so they work now :)

--- events.py
+++ events.py
@@ -1,6 +1,6 @@
 import pyev
 
-class Event(object):
+class TimeEvent(object):
     
     def __init__(self, after=0, interval=0, callback=None):
         self.after = after
@@ -12,3 +12,28 @@ class Event(object):
         self._timer.start()
         
 
+class IntervalEvent(TimeEvent):
+    def __init__(self, interval, callback):
+        self.after = interval
+        self.interval = interval
+        self.callback = callback
+            
+            
+class FDEvent(object):
+    def __init__(self, fd, events, callback):
+        self.fd = fd
+        self.callback = callback
+        self.events = events
+        
+    def install(self, reactor):
+        self._io = pyev.Io(self.fd, self.events, reactor._loop, self.callback, 0)
+        self._io.start()
+        
+class ReadEvent(FDEvent):
+    def __init__(self, fd, callback):
+        super(FDEvent, self).__init__(self, fd, pyev.EV_READ, callback)
+        
+        
+class WriteEvent(FDEvent):
+    def __init__(self, fd, callback):
+        super(FDEvent, self).__init__(self, fd, pyev.EV_WRITE, callback)
--- reactor.py
+++ reactor.py
@@ -2,7 +2,7 @@ import pyev
 import greenlet
 import time
 from handlers import SIGINTHandler
-from events import Event
+from events import IntervalEvent
 
 
 class Reactor(object):
@@ -11,23 +11,31 @@ class Reactor(object):
         self._loop = pyev.default_loop()
         self._current = greenlet.getcurrent()
         self._queue = []
+        self._handlers = []
+        self._events = []
         
     def run(self):
         #create dispatch event
         #create CTRL-C handler
         sigint_handler = SIGINTHandler()
         self.install(sigint_handler)
-        dispatch_event = Event(0,2,self.dispatch)
+        dispatch_event = IntervalEvent(2,self.dispatch)
         self.install(dispatch_event)
         #run loop
         self._loop.loop()
 
     def dispatch(self, watcher, events, data=None):
         print('switching')
-        self._current.switch()
+        current = self._queue.pop(0)
+        current.switch()
+        self._queue.append(current)
 
     def add(self, function):
         self._queue.append(greenlet.greenlet(function))
         
     def install(self, handler):
+        handler.install(self)
+        
+    def install_handler(self, handler):
+        self._handlers.append(handler)
         handler.install(self)
