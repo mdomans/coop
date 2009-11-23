5a78cfbf78778eabba7c51423fad39f1dd58f5b7
Michał Domański <michal.domanski@sensisoft.com>
2009-11-23 01:55:46 +0100

    some code structure cleanup

--- reactor.py
+++ reactor.py
@@ -5,27 +5,29 @@ from handlers import SIGINTHandler
 from events import Event
 
 
-def dispatch(watcher, events):
-    print('switching')
-    current = greenlet.getcurrent()
-    current.switch()
-
 class Reactor(object):
     
     def __init__(self):
         self._loop = pyev.default_loop()
+        self._current = greenlet.getcurrent()
         self._queue = []
         
     def run(self):
         #create dispatch event
         #create CTRL-C handler
         sigint_handler = SIGINTHandler()
-        sigint_handler.install(self)
-        dispatch_event = Event(0,2,dispatch)
-        dispatch_event.install(self)
+        self.install(sigint_handler)
+        dispatch_event = Event(0,2,self.dispatch)
+        self.install(dispatch_event)
         #run loop
         self._loop.loop()
+
+    def dispatch(self, watcher, events, data=None):
+        print('switching')
+        self._current.switch()
 
     def add(self, function):
         self._queue.append(greenlet.greenlet(function))
         
+    def install(self, handler):
+        handler.install(self)
